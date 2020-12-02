4156c4886417247ee7a254998caa7307d0e9e6ab
Jesse Clark <hello@developerjesse.com>
2020-12-02 15:22:23 +0200

    Add warn setting for style errors. (#10)

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,11 @@ module.exports = {
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
     'react/prop-types': 'off',
-    'no-unused-vars': 'warn'
+    'indent': 'warn',
+    'no-unused-vars': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'no-unreachable': 'warn'
   },
   settings: {
     react: {
